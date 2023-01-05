ba90ce37344bd8a515a055face3df566e98f6789
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-01-05 09:55:10 -0500

    asdf

--- api-project/index.js
+++ api-project/index.js
@@ -1,13 +1,19 @@
-const URL = "https://api.quotable.io/random";
-const clashroyalecards = "";
+const animalcrossing = "http://acnhapi.com/v1/villagers/";
 
-async function getData(URL) {
+async function getData(animalcrossing) {
   try {
-    const response = await fetch(URL);
-    const data = await response.json(); //makes the data in a JSON object we can use
-    document.getElementById("api-response").textContent = data.content;
+    const response = await fetch(animalcrossing);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(response);
+    } else {
+      const data = await response.json(); //makes the data in a JSON object we can use
+      document.getElementById("api-response").textContent = data.content;
+
+      console.log("amazing");
+    }
   } catch (error) {
     console.log(error);
+    console.log("bad");
   }
 }
-getData(URL);
+getData(animalcrossing);
