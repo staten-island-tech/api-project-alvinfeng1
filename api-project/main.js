b0d0e479cbc04baaeefe84dba026fc257e1fe656
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-01-11 10:22:35 -0500

    f

--- api-project/main.js
+++ api-project/main.js
@@ -1,17 +1,19 @@
-const villagername = `Cyrano`
-const URL = `http://acnhapi.com/v1/villagers/${villagername}`;
+const villager = ``;
+const villagers = `http://acnhapi.com/v1/villagers/${villager}`;
 
-async function getData(URL) {
+async function getData(villagers) {
   try {
-    const response = await fetch(URL);
+    const response = await fetch(villagers);
     if (response.status < 200 || response.status > 299) {
       throw new Error(response);
     } else {
       const data = await response.json(); //makes the data in a JSON object we can use
+      data = JSON.parse(data);
+      console.log(data);
       data.forEach((name) => {
         document.getElementById("api-reponse").insertAdjacentElement(
           "afterbegin",
-          `<div class="card"> <h2 class="villagername">${name - USen}</h2>
+          `<div class="card"> <h2 class="villagername">${name.name - USen}</h2>
              img src="${image.jpg.large_image_url}" alt="" /></div>`
         );
       });
@@ -23,4 +25,4 @@ async function getData(URL) {
     console.log("bad");
   }
 }
-getData(animalcrossing);
+getData(villagers);
