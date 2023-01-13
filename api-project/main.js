5c6bcd5e7d53b77d9c2f8492d5b4bbf739ff0ea2
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-01-13 10:15:09 -0500

    klh

--- api-project/main.js
+++ api-project/main.js
@@ -1,31 +1,25 @@
-const villager = ``;
-const villagers = `http://acnhapi.com/v1/villagers/${villager}`;
+const amiibo = `https://www.amiiboapi.com/api/amiibo/`;
 
-async function getData(villagers) {
+async function getData(amiibo) {
   try {
-    const response = await fetch(villagers);
+    const response = await fetch(amiibo);
     if (response.status < 200 || response.status > 299) {
       throw new Error(response);
     } else {
       const data = await response.json(); //makes the data in a JSON object we can use
       console.log(data);
-      data.forEach((Object) => {
+      data.forEach((data) => {
         document.getElementById("api-reponse").insertAdjacentElement(
-          "afterbegin",
-          `<div class="card"> <h2 class="villagername">${
-            Object.name.name - USen
-          }</h2>
-             img class="image" src="${
-               Object.image.jpg.large_image_url
-             }"/></div>`
+          "beforeend",
+          `<div class="card"> <h2 class="amiibo">${data.amiibo.name}</h2>
+             img class="image" src="${data.amiibo.image.jpg.large_image_url}"/></div>`
         );
       });
       console.log("good");
-      console.log(data);
     }
   } catch (error) {
     console.log(error);
     console.log("bad");
   }
 }
-getData(villagers);
+getData(amiibo);
